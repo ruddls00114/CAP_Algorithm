46345be972fce35aa1cd0105fb3c7c22f188a136
kyoungin <kkmm114@gmail.com>
2018-01-31 00:59:45 +0900

    if문 예제

--- algorithm/algorithm/0_test.c
+++ algorithm/algorithm/0_test.c
@@ -1,7 +1,18 @@
 #include<stdio.h>
 
 int main(){
-	int score;
+	int A, B, C, second=0;
+	scanf_s("%d %d %d", &A, &B, &C);
+	if (A > B){
+		if (B > C)	second = B;
+		else second = C;
+	}
+	else {
+		if (A>C)	second = A;
+		else second = C;
+	}
+	printf(second);
+	/*int score;
 	scanf("%d", &score);
 	if (score >= 90)
 		printf("A");
@@ -12,5 +23,5 @@ int main(){
 	else if (score >= 60)
 		printf("D");
 	else
-		printf("F");
+		printf("F");*/
 }
