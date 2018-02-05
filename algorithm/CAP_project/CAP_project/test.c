1dcacc69a32afc9d4fc6a7e48a98810206771ef1
kyoungin <kkmm114@gmail.com>
2018-02-05 22:11:23 +0900

    second

---
+++ algorithm/CAP_project/CAP_project/test.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(){
+	/*별찍기2
+	출력
+	     *
+	    **
+	   ***
+	*/
+	int n;
+	scanf("%d", &n);
+	for (int i = 1; i <= n; i++){
+		for (int j = n - 1; j >= i; j--){	//공백의 갯수하나씩 줄이기 -> j>=i
+			printf(" ");
+		}
+		for (int k = 1; k <= i; k++){
+			printf("*");
+		}
+		printf("\n");
+
+	}
+	getchar();
+	
+}
