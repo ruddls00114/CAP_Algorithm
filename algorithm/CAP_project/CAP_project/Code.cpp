d9ebc7fe89ff719068d4ea68e322862661d84df4
kyoungin <kkmm114@gmail.com>
2018-03-01 15:26:39 +0900

    음게 cpp

--- algorithm/CAP_project/CAP_project/Code.cpp
+++ algorithm/CAP_project/CAP_project/Code.cpp
@@ -1,15 +1,32 @@
 #include<iostream>
+#include<string>
+#include<array>
 #define IOFAST() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
 using namespace std;
-
+bool arr(int a[], int b[]);
 int main(){
-	//10039 - 평균점수
-	int n = 5,input,sum=0;
-	while (n--){
-		cin >> input;
-		if (input < 40) input = 40;
-		sum += input;
-	}
-	cout << (sum / 5);
 
+	//2029- 음계
+	int i=0,a[8], b[] = { 1, 2, 3, 4, 5, 6, 7, 8 }, c[] = {8,7,6,5,4,3,2,1};
+	string temp="";
+	for (i = 0; i < 8; i++){
+		cin >> a[i];
+	}
+	if (arr(a, b)) cout << "ascending";
+	else if (arr(a, c)) cout << "descending";
+	else cout << "mixed";
+}
+
+bool arr(int a[], int b[]){
+
+	int i = 0;
+	//if (a.size() != b.size())	return false;
+	while (i< 8){
+		if (a[i] != b[i]){
+			break;
+		}
+		i++;
+	}
+	if (i == 8) return true;
+	else return false;
 }
