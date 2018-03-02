4c53df74eed2eaca3c8060f3b626e6352687f5ea
kyoungin <kkmm114@gmail.com>
2018-03-02 23:46:00 +0900

    피보나치수열 2

--- algorithm/CAP_project/CAP_project/Code.cpp
+++ algorithm/CAP_project/CAP_project/Code.cpp
@@ -1,21 +1,25 @@
 	#include<iostream>
-	#include<string>
-	#include<array>
 	#define IOFAST() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
 	using namespace std;
 	/*
 	DP 예 피보나치수열 
 	*/
-	//1.반복문
+	//2.재귀, 메모이제이션
+	int m[46];
+	int r(int i);
+
 	int main(){
-		int n, v[46];
-		for (int i = 0; i <= 45; i++){
-			if (i == 0 || i == 1)v[i] = i;
-			else{
-				v[i] = v[i - 1] + v[i - 2];
-			}
-		}
-		cin >> n;
-		cout << v[n];
-	}}
-	
+		int n;
+		memset(m, 0, sizeof(m));
+	cin >> n;
+	cout << r(n);
+	}
+
+	int r(int i){
+		if (i == 0 || i == 1)
+			return m[i]=i;
+		else if(m[i]) 
+			return m[i];
+		else
+			return m[i]=r(i - 1) + r(i - 2);
+	}
