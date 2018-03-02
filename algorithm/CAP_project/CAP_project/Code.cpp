aaee0c64929f3a18839fd8fb9ebda837e3d3b4fb
kyoungin <kkmm114@gmail.com>
2018-03-02 23:10:22 +0900

    피보나치수열 1

--- algorithm/CAP_project/CAP_project/Code.cpp
+++ algorithm/CAP_project/CAP_project/Code.cpp
@@ -1,32 +1,21 @@
-#include<iostream>
-#include<string>
-#include<array>
-#define IOFAST() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
-using namespace std;
-bool arr(int a[], int b[]);
-int main(){
-
-	//2029- 음계
-	int i=0,a[8], b[] = { 1, 2, 3, 4, 5, 6, 7, 8 }, c[] = {8,7,6,5,4,3,2,1};
-	string temp="";
-	for (i = 0; i < 8; i++){
-		cin >> a[i];
-	}
-	if (arr(a, b)) cout << "ascending";
-	else if (arr(a, c)) cout << "descending";
-	else cout << "mixed";
-}
-
-bool arr(int a[], int b[]){
-
-	int i = 0;
-	//if (a.size() != b.size())	return false;
-	while (i< 8){
-		if (a[i] != b[i]){
-			break;
+	#include<iostream>
+	#include<string>
+	#include<array>
+	#define IOFAST() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+	using namespace std;
+	/*
+	DP 예 피보나치수열 
+	*/
+	//1.반복문
+	int main(){
+		int n, v[46];
+		for (int i = 0; i <= 45; i++){
+			if (i == 0 || i == 1)v[i] = i;
+			else{
+				v[i] = v[i - 1] + v[i - 2];
+			}
 		}
-		i++;
-	}
-	if (i == 8) return true;
-	else return false;
-}
+		cin >> n;
+		cout << v[n];
+	}}
+	
