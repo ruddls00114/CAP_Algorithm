5c7c5706eb95aff42bd8cdcafd280c036630503e
kyoungin <kkmm114@gmail.com>
2018-03-03 21:41:45 +0900

    ...미완성

--- algorithm/CAP_project/CAP_project/Code.cpp
+++ algorithm/CAP_project/CAP_project/Code.cpp
@@ -1,25 +1,39 @@
-	#include<iostream>
-	#define IOFAST() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
-	using namespace std;
-	/*
-	DP ¿¹ ÇÇº¸³ªÄ¡¼ö¿­ 
-	*/
-	//2.Àç±Í, ¸Þ¸ðÀÌÁ¦ÀÌ¼Ç
-	int m[46];
-	int r(int i);
+ï»¿#include<iostream>
+#include<cstring>
+#define IOFAST() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+using namespace std;
 
-	int main(){
-		int n;
-		memset(m, 0, sizeof(m));
+long int a[90];
+int main(){
+	int n;
+	memset(a, 0, sizeof(a));
 	cin >> n;
-	cout << r(n);
+	a[0] = 0, a[1] = 1;
+	for (int i = 2; i <= n ; i++){
+		a[i ] = a[i -1] + a[i-2];
 	}
+	cout << a[n];
+}
+/*
+2748 ë²ˆ í”¼ë³´ë‚˜ì¹˜ ìˆ˜ì—´ ë¯¸ì™„ì„± 
 
-	int r(int i){
-		if (i == 0 || i == 1)
-			return m[i]=i;
-		else if(m[i]) 
-			return m[i];
-		else
-			return m[i]=r(i - 1) + r(i - 2);
-	}
+//top-down
+long int m[91];
+int r(int i);
+int main(){
+	int n;
+	memset(m, 0, sizeof(m));
+	scanf("%d", &n);
+	r(n);
+	printf("%lld",m[n]);
+}
+int r(int i){
+	if (m[i])
+		return m[i];
+	else if (i == 0 || i == 1)
+		return m[i] = i;
+	else
+		return m[i] = r(i - 1) + r(i - 2);
+
+}
+*/
