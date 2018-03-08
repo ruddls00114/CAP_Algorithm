ea8157c8a19b9d1bae300feb54af4d554edf6e51
kyoungin <kkmm114@gmail.com>
2018-03-08 19:29:51 +0900

    1로만들기 bottom-up

--- algorithm/CAP_project/CAP_project/Code.cpp
+++ algorithm/CAP_project/CAP_project/Code.cpp
@@ -1,42 +1,33 @@
 ﻿#include<iostream>
 #include<cstring>
 #define IOFAST() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
-#define min(a,b) a<b?a:b
+#include<algorithm>
 using namespace std;
-int dp[4][1002];
-int data[4][1002];
+//20180308 1로만들기
+//
+//X가 3으로 나누어 떨어지면, 3으로 나눈다.
+//X가 2로 나누어 떨어지면, 2로 나눈다.
+//1을 뺀다.
+//정수 N이 주어졌을 때, 위와 같은 연산 세 개를 적절히 사용해서 1을 만들려고 한다.
+//연산을 사용하는 횟수의 최소값을 출력하시오.
 
+//bottom-up 사용하기
+int dp[1000001];
 int main(){
-	int i, j, n, min;
-	//memset(data[],-1, sizeof(data));
-	//memset(dp, 0, sizeof(dp));
+	int n; 
+	scanf("%d", &n);
+	dp[1] = 0;
+	for (int i = 2; i <= n; i++){
 
-	cin >> n;
-	for (j = 1; j <= n; j++){
-		for (i = 1; i <= 3; i++){
-			cin >> data[j][i];
+		dp[i] = dp[i - 1] + 1;	//1 뺀 경우 
+
+		if (i % 3==0) {
+			dp[i] = min(dp[i / 3] + 1, dp[i]); 
+		}
+		if (i % 2==0){
+			dp[i] = min(dp[i / 2] + 1, dp[i]);
+
 		}
 	}
-	for (j = 1; j <= n; j++){
-		if (j == 1){
-			dp[j][1] = data[j][1];
-			dp[j][2] = data[j][2];
-			dp[j][3] = data[j][3];
-			continue;
-		}
-		dp[j][1] = min(dp[j - 1][2] + data[j][1], dp[j - 1][3] + data[j][1]);
-		dp[j][2] = min(dp[j - 1][1] + data[j][2], dp[j - 1][3] + data[j][2]);
-		dp[j][3] = min(dp[j - 1][1] + data[j][3], dp[j - 1][2] + data[j][3]);
-
-	}
-
-
-	for (j = 1; j <= n; j++){
-		for (i = 1; i <= 3; i++){
-			cout << dp[j][i] << " ";
-		}
-		cout << endl;
-	}
-	min = min(min(dp[n][1], dp[n][2]), dp[n][3]);
-	cout << min;
+	printf("%d\n", dp[n]);
 }
