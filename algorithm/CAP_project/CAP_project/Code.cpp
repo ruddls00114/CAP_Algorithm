648fe3b109c2f4b0f0645f07d9bc8cd89f33cce7
kyoungin <kkmm114@gmail.com>
2018-03-08 19:56:26 +0900

    1로만들기 Top-down

--- algorithm/CAP_project/CAP_project/Code.cpp
+++ algorithm/CAP_project/CAP_project/Code.cpp
@@ -1,7 +1,9 @@
 ﻿#include<iostream>
 #include<cstring>
 #define IOFAST() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+//#define min(a,b) a<b?a:b;
 #include<algorithm>
+
 using namespace std;
 //20180308 1로만들기
 //
@@ -11,23 +13,21 @@ using namespace std;
 //정수 N이 주어졌을 때, 위와 같은 연산 세 개를 적절히 사용해서 1을 만들려고 한다.
 //연산을 사용하는 횟수의 최소값을 출력하시오.
 
-//bottom-up 사용하기
+//top-down 사용하기
 int dp[1000001];
+int f(int n){
+	if (n == 0 || n == 1) return 0;
+	if (dp[n])
+		return dp[n];
+	else
+		return dp[n] = min(min(f(n / 3) + (n % 3 * 10) + 1, f(n / 2) + (n % 2 * 10) + 1), f(n - 1) + 1);
+
+}
+
 int main(){
 	int n; 
 	scanf("%d", &n);
 	dp[1] = 0;
-	for (int i = 2; i <= n; i++){
-
-		dp[i] = dp[i - 1] + 1;	//1 뺀 경우 
-
-		if (i % 3==0) {
-			dp[i] = min(dp[i / 3] + 1, dp[i]); 
-		}
-		if (i % 2==0){
-			dp[i] = min(dp[i / 2] + 1, dp[i]);
-
-		}
-	}
-	printf("%d\n", dp[n]);
+	f(n);
+	printf("%d\n",dp[n]);
 }
