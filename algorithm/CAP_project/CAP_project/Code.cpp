d0509f7efa5567ba7d3099601e6fbc87e446c6ce
kyoungin <kkmm114@gmail.com>
2018-03-12 18:30:20 +0900

    동전 포기

--- algorithm/CAP_project/CAP_project/Code.cpp
+++ algorithm/CAP_project/CAP_project/Code.cpp
@@ -4,33 +4,25 @@
 #include<algorithm>
 
 using namespace std;
-int dp[501][501], data[501][501];
+
 
 int main(){
-	/*
-	5
-	7
-	3 8         //10 15
-	8 1 0       //18 16 8
-	2 7 4 4     //
-	4 5 2 6 5   //
-	*/
-	int n, maxNum = 0;
-	cin >> n;
-	for (int i = 1; i <= n; i++){
-		for (int j = 1; j <= i; j++){
-			cin >> data[i][j];
-		}
-	}
-	dp[1][1] = data[1][1];
-	for (int i = 2; i <= n; i++){
-		for (int j = 1; j <= i; j++){
-			dp[i][j] = data[i][j] + max(dp[i - 1][j - 1], dp[i - 1][j]);
 
+	int n, k, i, j;
+	cin >> n >> k;
+	int* dp = new int[k + 1];
+	int* values = new int[n + 1];
+	memset(dp, 0, sizeof(dp));
+	memset(values, 0, sizeof(values));
+	for (i = 1; i <= n; i++){
+		cin >> values[i];
+	}
+	dp[1] = 1;
+	for (i = 1; i <= n; i++){
+		for (j = 1; j <= k; j++){
+			if (j >= values[i])
+				dp[j] +=dp[j-values[i-1]];
 		}
 	}
-	for (int i = 1; i <= n; i++){
-		maxNum = max(maxNum, dp[n][i]);
-	}
-	cout << maxNum;
+	cout << dp[k] << endl;
 }
