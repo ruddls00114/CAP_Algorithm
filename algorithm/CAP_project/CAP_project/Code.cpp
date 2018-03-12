ffa0ac56f4ab041c4482a1254be64a60f0e72735
kyoungin <kkmm114@gmail.com>
2018-03-12 16:49:27 +0900

    숫자삼각형

--- algorithm/CAP_project/CAP_project/Code.cpp
+++ algorithm/CAP_project/CAP_project/Code.cpp
@@ -1,33 +1,36 @@
 ﻿#include<iostream>
 #include<cstring>
 #define IOFAST() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
-//#define min(a,b) a<b?a:b;
 #include<algorithm>
 
 using namespace std;
-//20180308 1로만들기
-//
-//X가 3으로 나누어 떨어지면, 3으로 나눈다.
-//X가 2로 나누어 떨어지면, 2로 나눈다.
-//1을 뺀다.
-//정수 N이 주어졌을 때, 위와 같은 연산 세 개를 적절히 사용해서 1을 만들려고 한다.
-//연산을 사용하는 횟수의 최소값을 출력하시오.
-
-//top-down 사용하기
-int dp[1000001];
-int f(int n){
-	if (n == 0 || n == 1) return 0;
-	if (dp[n])
-		return dp[n];
-	else
-		return dp[n] = min(min(f(n / 3) + (n % 3 * 10) + 1, f(n / 2) + (n % 2 * 10) + 1), f(n - 1) + 1);
-
-}
+int dp[501][501], data[501][501];
 
 int main(){
-	int n; 
-	scanf("%d", &n);
-	dp[1] = 0;
-	f(n);
-	printf("%d\n",dp[n]);
+	/*
+	5
+	7
+	3 8         //10 15
+	8 1 0       //18 16 8
+	2 7 4 4     //
+	4 5 2 6 5   //
+	*/
+	int n, maxNum = 0;
+	cin >> n;
+	for (int i = 1; i <= n; i++){
+		for (int j = 1; j <= i; j++){
+			cin >> data[i][j];
+		}
+	}
+	dp[1][1] = data[1][1];
+	for (int i = 2; i <= n; i++){
+		for (int j = 1; j <= i; j++){
+			dp[i][j] = data[i][j] + max(dp[i - 1][j - 1], dp[i - 1][j]);
+
+		}
+	}
+	for (int i = 1; i <= n; i++){
+		maxNum = max(maxNum, dp[n][i]);
+	}
+	cout << maxNum;
 }
