81f4642a4cba9163964281d2881784ecc6df9e4d
kyoungin <kkmm114@gmail.com>
2018-03-26 16:23:01 +0900

    붕어빵 판매하기 완성

--- algorithm/CAP_project/CAP_project/Code.cpp
+++ algorithm/CAP_project/CAP_project/Code.cpp
@@ -1,32 +1,62 @@
-﻿#include<iostream>
+﻿//#include<iostream>
+//#include<cstring>
+//#define IOFAST() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+//#include<algorithm>
+//
+//using namespace std;
+//int dp[10001];// 해빈이가 얻을 수 있는 최대 수익
+////int p[10001];
+//
+//
+//int main(){
+//
+//	int n, i,j;
+//	scanf("%d", &n);
+//	int* p = new int[n+1];
+//	int* dp = new int[n + 1];
+//	memset(p, 0, sizeof(p));
+//	memset(dp, 0, sizeof(dp));
+//	for (i = 1; i <= n; i++){
+//		scanf("%d", p+i);
+//		dp[i] = p[i];		
+//	}
+//	/*
+//	for (j = 2; j <= n; j++){
+//		for (i = 2; i <= j; i++){
+//			dp[j] = max(dp[j],(dp[i - 1] + dp[j - i + 1]));
+//		}
+//	}
+//*/
+//	for (j = 2; j <= n; j++){
+//		for (i = 2; i <= j; i++){
+//			dp[j] = max(dp[j], (dp[i - 1] + dp[j - i + 1]));
+//		}
+//	}
+//
+//	printf("%d", dp[n]);
+//}
+#include<iostream>
 #include<cstring>
 #define IOFAST() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
 #include<algorithm>
 
 using namespace std;
-int dp[10001];// 해빈이가 얻을 수 있는 최대 수익
-int data[10001];
-
-//void f(int n){
-//	int i;
-//	for (i = n; i >= 2; i++){
-//		dp[n] = max(dp[n], data[i - 1] + data[n - i + 1]);
-//	}
-//}
-
 
 int main(){
 
-	int n, i;
+	int n, i, j;
 	scanf("%d", &n);
+	int* dp = new int[n + 1];
+	memset(dp, 0, sizeof(dp));
 	for (i = 1; i <= n; i++){
-		scanf("%d", data+i);
-		dp[i] = data[i];
+		scanf("%d", dp + i);
+
 	}
 
-	//printf("%d", dp[2]);
-	for (i = 2; i <=n; i++){
-		dp[n] = max(dp[n], (data[i - 1] + data[n - i + 1]));
+	for (j = 2; j <= n; j++){
+		for (i = 2; i <= j; i++){
+			dp[j] = max(dp[j], (dp[i - 1] + dp[j - i + 1]));
+		}
 	}
 
 	printf("%d", dp[n]);
