5ea09883f906562182137dcc6bf07dc32914bb64
kyoungin <kkmm114@gmail.com>
2018-03-26 19:00:39 +0900

    계단오르기 미완성

--- algorithm/CAP_project/CAP_project/Code.cpp
+++ algorithm/CAP_project/CAP_project/Code.cpp
@@ -1,63 +1,33 @@
-﻿//#include<iostream>
-//#include<cstring>
-//#define IOFAST() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
-//#include<algorithm>
-//
-//using namespace std;
-//int dp[10001];// 해빈이가 얻을 수 있는 최대 수익
-////int p[10001];
-//
-//
-//int main(){
-//
-//	int n, i,j;
-//	scanf("%d", &n);
-//	int* p = new int[n+1];
-//	int* dp = new int[n + 1];
-//	memset(p, 0, sizeof(p));
-//	memset(dp, 0, sizeof(dp));
-//	for (i = 1; i <= n; i++){
-//		scanf("%d", p+i);
-//		dp[i] = p[i];		
-//	}
-//	/*
-//	for (j = 2; j <= n; j++){
-//		for (i = 2; i <= j; i++){
-//			dp[j] = max(dp[j],(dp[i - 1] + dp[j - i + 1]));
-//		}
-//	}
-//*/
-//	for (j = 2; j <= n; j++){
-//		for (i = 2; i <= j; i++){
-//			dp[j] = max(dp[j], (dp[i - 1] + dp[j - i + 1]));
-//		}
-//	}
-//
-//	printf("%d", dp[n]);
-//}
-#include<iostream>
+﻿#include<iostream>
 #include<cstring>
 #define IOFAST() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
 #include<algorithm>
-
-using namespace std;
-
+#define max(a,b) a>b?a:b 
+/*계단은 한 번에 한 계단씩 또는 두 계단씩 오를 수 있다.
+즉, 한 계단을 밟으면서 이어서 다음 계단이나, 다음 다음 계단으로 오를 수 있다.
+연속된 세 개의 계단을 모두 밟아서는 안된다. 단, 시작점은 계단에 포함되지 않는다.
+마지막 도착 계단은 반드시 밟아야 한다.*/
+int dp[301];
 int main(){
 
-	int n, i, j;
+	int n, i;
 	scanf("%d", &n);
-	int* dp = new int[n + 1];
-	memset(dp, 0, sizeof(dp));
-	for (i = 1; i <= n; i++){
-		scanf("%d", dp + i);
+	int* data = new int[n+1];
+	memset(data, 0, sizeof(data));
+	for (i = 1; i <= n; i++)
+		scanf("%d", data+i);
 
+	dp[1] = data[1];
+	
+	for (i = 2; i < n; i++){
+		//dp[i] = max(dp[i - 1] + dp[i - 2], dp[i - 2] + dp[i]);
+//		dp[i] = dp[i - 1] + data[i] > dp[i - 1] + data[i + 1] ? dp[i - 1] + data[i] : dp[i - 1];
+		dp[i] = dp[i - 1] + data[i] > dp[i - 1] + data[i + 1] ? dp[i - 1] + data[i] : dp[i - 1];
+
+			printf("dp[%d] --> %d \n", i, dp[i]);
 	}
 
-	for (j = 2; j <= n; j++){
-		for (i = 2; i <= j; i++){
-			dp[j] = max(dp[j], (dp[i - 1] + dp[j - i + 1]));
-		}
-	}
-
+	dp[n] = data[n] + dp[n - 1];
 	printf("%d", dp[n]);
+
 }
