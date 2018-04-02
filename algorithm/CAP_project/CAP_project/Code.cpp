97f39c75cf681b06fc739ae7ce7d5206cf9655da
kyoungin <kkmm114@gmail.com>
2018-04-02 18:27:16 +0900

    정렬

--- algorithm/CAP_project/CAP_project/Code.cpp
+++ algorithm/CAP_project/CAP_project/Code.cpp
@@ -1,26 +1,38 @@
-﻿#include<iostream>
-#include<cstring>
-#define IOFAST() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
-#include<algorithm>
-#define max(a,b) a>b?a:b
-int main(){
-	int n, i;
-	scanf("%d", &n);
-	int* data = new int[n + 1];
-	int* dp = new int[n + 1];
-	memset(data, 0, sizeof(data));
-	memset(dp, 0, sizeof(dp));
-	//반례  7  7  1  5  7  7  1 3  정답31 내답29
-		//      
+﻿	#include<iostream>
+	#include<cstring>
+	#define IOFAST() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+	#include<algorithm>
 
+	int main(){
 
+		int n,flag, i,j,tm;
+		scanf("%d", &n);
+		int* arr = new int[n];
+		for (i = 0; i < n; i++)	
+			scanf("%d", arr+i);
+		//버블정렬
+		for (j = 0; j < n; j++){
+			flag = 0;
+			for (i =1; i < n - j; i++){
+				if (arr[i - 1] >= arr[i]){
+					tm = arr[i - 1];
+					arr[i - 1] = arr[i];
+					arr[i] = tm;
+					flag = 1;
+				}
+			}
+			if (flag == 0) break;
+		}
+		/* //삽입정렬
+		for (i = 1; i < n; i++){		
+			key = arr[i];
+			for (j = i - 1; j >= 0; j--){ 
+				if (arr[j] <= key)	break;
+				arr[j + 1] = arr[j];
+			}
+			arr[j + 1] = key;
+		} */
+		for (i = 0; i < n; i++)	
+			printf("%d \n", *(arr + i));
 
-	for (i = 1; i <= n; i++)	scanf("%d", data + i);
-	dp[1] = data[1];
-	dp[2] = data[1] + data[2];
-	for (i = 3; i <= n; i++){
-		dp[i] = max(data[i] + data[i - 1] + dp[i - 3] , data[i] + dp[i - 2]);
-		dp[i] = dp[i-1] > dp[i] ? dp[i-1] : dp[i];
 	}
-	printf("%d", dp[n]);
-}
