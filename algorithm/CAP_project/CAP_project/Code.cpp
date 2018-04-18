db18516a03739ec6af9cc550bab1f0fc4afb497c
kyoungin <kkmm114@gmail.com>
2018-04-18 17:55:09 +0900

    최대공약수cpp

--- algorithm/CAP_project/CAP_project/Code.cpp
+++ algorithm/CAP_project/CAP_project/Code.cpp
@@ -1,58 +1,32 @@
-﻿	#include<iostream>
-	#include<cstring>
-	#define IOFAST() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
-	#include<algorithm>
-
-void mergeSort(int* arr, int first, int last);
-void merge(int* arr, int first,int middle, int last);
-
-int temp[1000001];
-
-	int main(){
-
-		int n, i;
-		scanf("%d", &n);
-		int* arr = new int[n];
-		for (i = 0; i < n; i++)	
-			scanf("%d", arr+i);
-		
-		mergeSort(arr, 0, n - 1);
-
-		for (i = 0; i < n; i++)
-			printf("%d \n", *(arr + i));
-
+﻿#include<iostream>
+#include<cstring>
+#include<algorithm>
+#include<math.h>
+#define IOFAST() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+using namespace std;
+long long int gcd(long long int h, long long int l);
+int main(){
+	long long int arr[2], gcdNum;
+	int i;
+	for (i = 0; i < 2; i++){
+		cin >> arr[i];
 	}
-	void mergeSort(int* arr, int first, int last){
-		int middle = (first + last) / 2;
-		if (first < last){
-			mergeSort(arr, first, middle);
-			mergeSort(arr, middle + 1, last);
-			merge(arr, first, middle, last);
-		}
+	gcdNum = gcd(arr[0], arr[1]);
+	for (i = 1; i <= gcdNum; i++){
+		cout << "1";
 	}
-	void merge(int* arr, int first, int middle, int last){
-		int i, j, k = first , p;
-		i = first;	//앞덩어리의 시작
-		j = middle + 1;//뒷더엉리의 시작
-
-		while (i <= middle && j <= last){ //두 배열이 끝에 닿을때 까지 반복.
-			temp[k] = arr[i] > arr[j] ? arr[j++] : arr[i++];
-			k++;
-
-		}
-		if (i > middle){
-			for (p = j; p <= last; p++){
-				temp[k] = arr[p];
-				k++;
-			}
-		}
-		else if (j > last){
-			for (p = i; p <= middle; p++){
-				temp[k] = arr[p];
-				k++;
-			}
-		}
-		for (int m = 0; m <= last; m++){
-			arr[m] = temp[m];
-		}
+}
+long long gcd(long long int h, long long int l){
+	long long int r, tmp;
+	if (h < l){
+		tmp = h;
+		h = l;
+		l = tmp;
 	}
+	do{
+		r = h%l;
+		h = l;
+		l = r;
+	} while (r != 0);
+	return h;
+}
