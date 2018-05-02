ab281589ae78b503d4776ec4549fc18c4e5b4cad
kyoungin <kkmm114@gmail.com>
2018-05-02 17:34:29 +0900

    ACM craft 미완성

--- algorithm/CAP_project/CAP_project/Code.cpp
+++ algorithm/CAP_project/CAP_project/Code.cpp
@@ -1,19 +1,41 @@
 ﻿#include<iostream>
 #include<cstring>
 #include<algorithm>
-#include<fstream>
 
 #define IOFAST() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
-#define MAXSIZE 101
+
 using namespace std;
-
 int main(){
+	IOFAST();
+	int t, i, j;
+	int a, b;
+	cin >> t;
+	for (i = 0; i < t; i++){
+		int n, k, w;
+		cin >> n >> k;
+		int * d = new int[n + 1];
+		int * dp = new int[n + 1];
+		memset(dp, 0, sizeof(dp));
 
-		cout << "|\\\_/|" << endl;
-		cout << "|q p|   /}" << endl;
-		cout << "( 0 )\"\"\"\\" << endl;
-		cout << "|\"^\"`    |" << endl;
-		cout << "||\_/=\\\\\_\_|" << endl;
+		for (j = 1; j <= n; j++){
+			cin >> d[j];
+			dp[j] = d[j];
+		}
 
-return 0;
+		for (j = 1; j <= k; j++){
+			int temp;
+			cin >> a >> b;
+		/*	if(a>b){
+				temp = a;
+				a = b;
+				b = temp;
+			}*/
+			dp[b] = max((dp[a] + d[b]), dp[b]);
+		}
+		cin >> w;
+		cout << dp[w] << '\n';
+
+	}//for t
+
+	return 0;
 }
