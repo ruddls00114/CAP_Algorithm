eb196ddd3cd13f302ba3ccc3bf8c78819e1a37fa
kyoungin <kkmm114@gmail.com>
2018-05-27 19:14:19 +0900

    백준 거스름 돈

--- algorithm/CAP_project/CAP_project/Code.cpp
+++ algorithm/CAP_project/CAP_project/Code.cpp
@@ -3,26 +3,21 @@
 #include<cstdlib>
 #define IOFAST() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
 using namespace std;
-int arr[9];
 int main(){
 	IOFAST();
-	
-	int n, i, j, sum  ;
-	for (j = 0; j < 3; j++){
-		sum = 0;
-		for (i = 0; i < 4; i++){
-			cin >> n;
-			sum += n;
+	int n, cnt = 0 ,k =500,flag=0;// flag =0 -> 5%%, 1 ->1&&
+	cin >> n;
+	n = 1000 - n;
+	while (n > 0){
+		cnt += n / k;
+		n -= (n / k)*k;
+		if (flag == 0) {
+			k /= 5; flag = 1;
 		}
-
-		switch (sum){
-		case 0: cout << "D\n"; break;
-		case 1: cout << "C\n"; break;
-		case 2: cout << "B\n"; break;
-		case 3: cout << "A\n"; break;
-		case 4: cout << "E\n"; break;
+		else {
+			k /= 2; flag = 0;
 		}
 	}
+	cout << cnt;
 	return 0;
-
 }
