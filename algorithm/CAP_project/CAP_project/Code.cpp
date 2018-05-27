0baa61b70c2781b0f86741f67853cb25d05e4a42
kyoungin <kkmm114@gmail.com>
2018-05-27 18:48:18 +0900

    백준 윷놀이

--- algorithm/CAP_project/CAP_project/Code.cpp
+++ algorithm/CAP_project/CAP_project/Code.cpp
@@ -1,17 +1,28 @@
 ﻿#include<iostream>
+#include<string>
+#include<cstdlib>
 #define IOFAST() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
 using namespace std;
-long long int a[91];
+int arr[9];
 int main(){
 	IOFAST();
-	int n, i;
-	cin >> n;
-	a[1] = 1;
-	for (i = 2; i <= n; i++){
-		a[i] = a[i - 1] + a[i-2];
-	}
+	
+	int n, i, j, sum  ;
+	for (j = 0; j < 3; j++){
+		sum = 0;
+		for (i = 0; i < 4; i++){
+			cin >> n;
+			sum += n;
+		}
 
-	cout << a[n];
+		switch (sum){
+		case 0: cout << "D\n"; break;
+		case 1: cout << "C\n"; break;
+		case 2: cout << "B\n"; break;
+		case 3: cout << "A\n"; break;
+		case 4: cout << "E\n"; break;
+		}
+	}
 	return 0;
 
 }
